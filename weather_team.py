44f816c6d0a58d2109443c236ddc0e81cccac486
Marianne O'Connor <marianneoco@gmail.com>
2019-03-24 16:10:40 +0000

    WeatherAPI

--- weather_team.py
+++ weather_team.py
@@ -2,10 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask("weather_team")
 
+import requests
+
+endpoint = "http://api.openweathermap.org/data/2.5/weather"
+payload = {"q": "London, UK", "units":"metric", "appid":"7854c0a8d8d5259f9efb07224b491299"}
+response = requests.get(endpoint, params=payload)
+data = response.json()
+weather = data["weather"][0]["main"]
+
 @app.route("/")
 def main_page():
-    return render_template("weather_team.html")
-#HTML for main page goes WeatherTeam
+    return render_template("weather_team.html", weather=weather)
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
