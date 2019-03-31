a542f8052b910164def31d99a27bd4bc95ce30d4
IrisGia <39098423+irisgia@users.noreply.github.com>
2019-03-31 12:21:57 +0100

    Add files via upload
    added subscribe form + weather status

--- weather_team.py
+++ weather_team.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 app = Flask("weather_team")
 
@@ -15,6 +15,22 @@ temp = data["main"]["temp"]
 def main_page():
     return render_template("weather_team.html", weather=weather, temp=temp)
 
+#subscribe button redirects to subscribe form pages
+@app.route('/subscribe', methods=['GET', 'POST'])
+def subscribe():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('subscribe'))
+
+    # show the form, it wasn't submitted
+    return render_template('subscribe.html')
+
+
+
+
+#form subscribe
 @app.route("/signup", methods=["POST"])
 def sign_up():
     form_data = request.form
